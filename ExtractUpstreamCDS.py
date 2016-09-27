78f88d73e3b7f8e1490f6a28f0ca70d9ae2d512b
Alan Tu <alan.han.tu+c9@gmail.com>
2016-09-27 20:29:23 +0000

    added data validation function

--- ExtractUpstreamCDS.py
+++ ExtractUpstreamCDS.py
@@ -2,18 +2,37 @@ import sys
 import os
 from Bio import SeqIO
 from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio import pairwise2
+import pdb
+
 
 # Usage
-# python [FULL PATH OF ExtractedUstreamCDS.py] ["fasta" or "genbank"]
-# [FULL PATH OF GBK FILES TO EXTRACT]
+# python ExtractUpstreamCDS.py [number of bases upstream to cut]
 
 # to combine multiple .gbk files into 1 gbk
-#   navigate to directory containing gbk files
-#   cat *.gbk > filename.gbk
+# navigate to directory containing gbk files
+# cat *.gbk > filename.gbk
 
-# either fasta or genbank
-output_filetype = sys.argv[1]
-num_bp_upstreamcds = int(sys.argv[2])
+# python -m pdb -Werror myprogram.py
+
+num_bp_upstreamcds = int(sys.argv[1])
+
+def validate_cds(record, feature):
+    
+    saved_record = record
+    saved_feature = feature
+    
+    protein_in_file = str(feature.qualifiers.get('translation', 'no_translation')).strip('\'[]')
+    
+    cds_to_protein = str(feature.extract(record).seq.translate(to_stop = True))
+    
+    
+    # if protein_in_file != cds_to_protein:
+    #     print "oh snap"
+    #     #alignment = pairwise2.align.globalxx(translated_protein, cds_to_protein, one_alignment_only=True)
+        #print(pairwise2.format_alignment(*alignment[0]))
+    return
+
 
 def get_upstream_cds(fullpath, filename):
 
@@ -26,30 +45,26 @@ def get_upstream_cds(fullpath, filename):
             for feature in record.features:
                 if feature.type == "CDS":
                     
+                    validate_cds(record, feature)
                     
-                    translated_protein = str(feature.qualifiers.get('translation', 'no_translation')).strip('\'[]')
-                    cds_to_protein = str(feature.extract(record).seq.translate(to_stop = True))
-                    
+                    #get the CDS nucleotide locations
                     cds_location = feature.location
                     start = cds_location.start.position
                     end = cds_location.end.position
             
                     
+                    #create a SeqFeature object containing the location of where to extract
                     upstream_cds = SeqFeature(FeatureLocation(start-num_bp_upstreamcds, start))
                     
                     extracted_upstream_cds = upstream_cds.extract(record)
                     
                     if len(extracted_upstream_cds.seq) != num_bp_upstreamcds:
-                        print "upstream cds length is too short = " + str(len(extracted_upstream_cds.seq))
+                        asldkfjalksdjflasjdf = 0
+                        #print "upstream cds length is too short = " + str(len(extracted_upstream_cds.seq))
                     else:
                         extracted_cds_list.append(extracted_upstream_cds)
 
-    if output_filetype == "fasta":
-        SeqIO.write(extracted_cds_list, filename +".CDS.fasta", output_filetype)
-    elif output_filetype == "genbank":
-        SeqIO.write(extracted_cds_list, filename + ".CDS.gbk", output_filetype)
-    else:
-        print "use either 'fasta' or 'genbank' in first argument"
+        #SeqIO.write(extracted_cds_list, filename +".CDS.fasta", "fasta")
     return
 
 # creates a list of the files in this directory
