53bfb5d88c7a43f92f5b6fa84585951f4ec4e80a
Alan Tu <alan.han.tu+c9@gmail.com>
2016-09-27 17:13:58 +0000

    cleaned up code and narrowed functionality

--- ExtractUpstreamCDS.py
+++ ExtractUpstreamCDS.py
@@ -15,14 +15,10 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 output_filetype = sys.argv[1]
 num_bp_upstreamcds = int(sys.argv[2])
 
-
-
-    
 def get_upstream_cds(fullpath, filename):
-    #rint "Working on " + files + "..."
 
-    count = 0
     extracted_cds_list = []
+    
     # reads in a gbk and creates a Seqrecordord object
     for record in SeqIO.parse(fullpath, "genbank"):
         
@@ -34,33 +30,10 @@ def get_upstream_cds(fullpath, filename):
                     translated_protein = str(feature.qualifiers.get('translation', 'no_translation')).strip('\'[]')
                     cds_to_protein = str(feature.extract(record).seq.translate(to_stop = True))
                     
-                    if translated_protein != cds_to_protein:
-                        
-                        count = count + 1
-                        print record.id
-                        #print feature.location
-                        print count
-                        print translated_protein
-                        print cds_to_protein
-                    else:
-                        continue
-                    
                     cds_location = feature.location
                     start = cds_location.start.position
                     end = cds_location.end.position
-                    
-                    
-                    
-                    cds = SeqFeature(FeatureLocation(start, end))
-                    
-                    
-                    
-                    #print "\n"
-                    
-                    
-                    
-                    #translated sequence in ncbi is different from CDS extracted then translated
-                    
+            
                     
                     upstream_cds = SeqFeature(FeatureLocation(start-num_bp_upstreamcds, start))
                     
@@ -70,8 +43,7 @@ def get_upstream_cds(fullpath, filename):
                         print "upstream cds length is too short = " + str(len(extracted_upstream_cds.seq))
                     else:
                         extracted_cds_list.append(extracted_upstream_cds)
-                    # translate to double check  ?? WHy?
-                    
+
     if output_filetype == "fasta":
         SeqIO.write(extracted_cds_list, filename +
                     ".CDS.fasta", output_filetype)
@@ -79,21 +51,14 @@ def get_upstream_cds(fullpath, filename):
         SeqIO.write(extracted_cds_list, filename + ".CDS.gbk", output_filetype)
     else:
         print "use either 'fasta' or 'genbank' in first argument"
-    #print "Done with " + files + "..."
-    
     return
 
 # creates a list of the files in this directory
 raw_datadir_listing = os.listdir(os.getcwd())
+
 # loops over the list of files
 for files in raw_datadir_listing:
     if files.endswith('.gbk'):
         full_path = os.path.join(os.getcwd(), files)
         filename = os.path.splitext(files)[0]
         get_upstream_cds(full_path, filename)
-
-
-# finished_listing = os.listdir(directory)
-# for files in finished_listing:
-#     if files.endswith('.CDS.gbk'):
-#         os.rename(files, os.getcwd()+"/extracted_seq/"+files)
