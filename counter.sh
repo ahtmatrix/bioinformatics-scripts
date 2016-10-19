91dfd2828dba62f5cc82180ae057c7de54839b92
Alan Tu <alan.han.tu+c9@gmail.com>
2016-10-19 21:53:05 +0000

    added more flexible ways to count records

--- counter.sh
+++ counter.sh
@@ -12,3 +12,10 @@ grep ">" rna_30upstream_.CDS.TIS.fasta| wc -l
 
 echo "rna_30upstream_.CDS.fasta record count = "
 grep ">" rna_30upstream_.CDS.fasta| wc -l 
+
+
+echo "rna.gbk record count = "
+grep "//" rna.gbk | wc -l
+
+echo "viral.gbk record count = "
+grep "//" viral.gbk | wc -l
