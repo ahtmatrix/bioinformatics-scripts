5891f2bff7b53f5b91eeae3cd517fb88d5f4c8f9
Alan Tu <alan.han.tu+c9@gmail.com>
2016-10-19 21:22:17 +0000

    generalized count.py for 2 file types

--- count.py
+++ count.py
@@ -9,10 +9,10 @@ from Bio import BiopythonWarning
 import re
 
 
-def count_records(fullpath, filename):
+def count_records(fullpath, filename, filetype):
 
     num_records = 0
-    for record in SeqIO.parse(fullpath, "genbank"):
+    for record in SeqIO.parse(fullpath, filetype):
         num_records +=1
     return num_records
 
@@ -21,7 +21,11 @@ raw_datadir_listing = os.listdir(os.getcwd())
 
 # loops over the list of files
 for files in raw_datadir_listing:
+    full_path = os.path.join(os.getcwd(), files)
+    filename = os.path.splitext(files)[0]
+    
     if files.endswith('.gbk'):
-        full_path = os.path.join(os.getcwd(), files)
-        filename = os.path.splitext(files)[0]
-        print filename +" record count= "+ str(count_records(full_path, filename))
+        print filename +" record count= "+ str(count_records(full_path, filename, "genbank"))
+        
+    elif files.endswith('.fasta'):
+        print filename +" record count= "+ str(count_records(full_path, filename, "fasta"))
