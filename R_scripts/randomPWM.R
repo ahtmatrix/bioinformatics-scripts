16bd638c1dc29afa4c67d2f30fd5a40974040a3c
ahtmatrix <alan.han.tu+github@gmail.com>
2016-11-30 23:45:09 +0000

    increased speed by vectorization and preallocation

--- R_scripts/randomPWM.R
+++ R_scripts/randomPWM.R
@@ -1,24 +1,23 @@
+rm(list = ls())
 #randomly pick score out of human PWM
 
-# 
+#
 # randomly pick a S_NT_position and sum up each 1 million times
-# 
-# 
-# 
+#
+#
+#
 # graph score vs freq histogram
-# 
+#
 # should be exponential / poisson distrubtion
-# 
-
-
-
-
+#
 
 human.filename = "rna30upstream_and_CDS.fasta"
 
 #make a pwm of size 13
 human = readDNAStringSet(human.filename)
 human.kozak = DNAStringSet(subseq(human, start = 22, end = 34))
+
+
 
 for (x in 1:2) {
   human.pwm = NULL
@@ -32,35 +31,55 @@ for (x in 1:2) {
   
   human.pwm    = PWM(human.kozak, type = PWM.type)
   
-  num.random.samples = 50000
+  num.random.samples = 10000
   
-  raw.data <- list()
+  raw.data <- numeric(length = num.random.samples)
+  
   #loop unrolling
-  for (i in 1:num.random.samples) {
-    list.scores <- c()
-    list.scores <- c(list.scores, sample(human.pwm[1:4], 1))
-    list.scores <- c(list.scores, sample(human.pwm[5:8], 1))
-    list.scores <- c(list.scores, sample(human.pwm[9:12], 1))
-    list.scores <- c(list.scores, sample(human.pwm[13:16], 1))
-    list.scores <- c(list.scores, sample(human.pwm[17:20], 1))
-    list.scores <- c(list.scores, sample(human.pwm[21:24], 1))
-    list.scores <- c(list.scores, sample(human.pwm[25:28], 1))
-    list.scores <- c(list.scores, sample(human.pwm[29:32], 1))
-    list.scores <- c(list.scores, sample(human.pwm[33:36], 1))
-    list.scores <- c(list.scores, sample(human.pwm[37:40], 1))
-    list.scores <- c(list.scores, sample(human.pwm[41:44], 1))
-    list.scores <- c(list.scores, sample(human.pwm[45:48], 1))
-    list.scores <- c(list.scores, sample(human.pwm[49:52], 1))
-    
-    raw.data <- c(raw.data, sum(list.scores))
-  }
+  print(system.time(for (i in 1:length(raw.data)) {
+    raw.data[i] <- (
+      sample(human.pwm[1:4], 1) +
+        sample(human.pwm[5:8], 1) +
+        sample(human.pwm[9:12], 1) +
+        sample(human.pwm[13:16], 1) +
+        sample(human.pwm[17:20], 1) +
+        sample(human.pwm[21:24], 1) +
+        sample(human.pwm[25:28], 1) +
+        sample(human.pwm[29:32], 1) +
+        sample(human.pwm[33:36], 1) +
+        sample(human.pwm[37:40], 1) +
+        sample(human.pwm[41:44], 1) +
+        sample(human.pwm[45:48], 1) +
+        sample(human.pwm[49:52], 1)
+    )
+  }))
   
   unlisted.data <- unlist(raw.data)
-  
-  hist(
-    unlisted.data,
-    breaks = num.random.samples,
-    xlab = "scores",
-    main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
-  )
+  #par(mfrow = c(1, 2))
+  if (x == 1) {
+    hist(
+      unlisted.data,
+      freq = FALSE,
+      breaks = num.random.samples / 15,
+      xlab = "scores",
+      main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
+    )
+    lines(density(unlisted.data), col = "blue", lwd = 2)
+    
+    
+    #lines(density(unlisted.data, adjust = 2), col = "green", lwd = 2, lty = "dotted")
+  } else if (x == 2) {
+    hist(
+      unlisted.data,
+      freq = FALSE,
+      breaks = num.random.samples / 15,
+      xlab = "scores",
+      main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
+    )
+    lines(density(unlisted.data), col = "blue", lwd = 2)
+
+  }
 }
+
+
+lines(density(unlisted.data), col = "blue", lwd = 2)
