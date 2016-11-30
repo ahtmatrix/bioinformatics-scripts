850825d58ba320b127f5a5fd2a72be9357a21da0
ahtmatrix <alan.han.tu+github@gmail.com>
2016-11-30 21:28:52 +0000

    iterate through randomPWM parameters

--- R_scripts/randomPWM.R
+++ R_scripts/randomPWM.R
@@ -11,54 +11,56 @@
 # 
 
 
+
+
+
 human.filename = "rna30upstream_and_CDS.fasta"
 
 #make a pwm of size 13
 human = readDNAStringSet(human.filename)
-human.kozak = DNAStringSet(subseq(human, start = 22, end= 34))
-human.pwm    = PWM(human.kozak, type = 'log2probratio')
+human.kozak = DNAStringSet(subseq(human, start = 22, end = 34))
 
-
-
-list.scores <- c()
-
-x = seq(from = 1, to = 52, by = 4)
-y = seq(from = 4, to = 52, by = 4)
-
-x
-y
-
-list.scores <- c(list.scores, sample(human.pwm[x:y],1))
-
-list.scores <- c(list.scores, sample(human.pwm[1:4],1))
-list.scores <- c(list.scores, sample(human.pwm[5:8],1))
-list.scores <- c(list.scores, sample(human.pwm[9:12],1))
-list.scores <- c(list.scores, sample(human.pwm[13:16],1))
-list.scores <- c(list.scores, sample(human.pwm[17:20],1))
-list.scores <- c(list.scores, sample(human.pwm[21:24],1))
-list.scores <- c(list.scores, sample(human.pwm[25:28],1))
-list.scores <- c(list.scores, sample(human.pwm[29:32],1))
-list.scores <- c(list.scores, sample(human.pwm[33:36],1))
-list.scores <- c(list.scores, sample(human.pwm[37:40],1))
-list.scores <- c(list.scores, sample(human.pwm[41:44],1))
-list.scores <- c(list.scores, sample(human.pwm[45:48],1))
-list.scores <- c(list.scores, sample(human.pwm[49:52],1))
-
-
-raw.data <- data.frame()
-for (i in 1:1000000000) {
-  raw.data <- rbind(raw.data, sum(list.scores))
-}
-
-
-
-list.scores
+for (x in 1:2) {
+  human.pwm = NULL
+  raw.data = NULL
+  unlisted.data = NULL
+  if (x == 1) {
+    PWM.type = "log2probratio"
+  } else if (x == 2) {
+    PWM.type = "prob"
+  }
   
-length(human.pwm)
-
-dank <- human.pwm[1:4]
-dank <- human.pwm[5:8]
-dank
-
-
-sample(dank, 1)
+  human.pwm    = PWM(human.kozak, type = PWM.type)
+  
+  num.random.samples = 50000
+  
+  raw.data <- list()
+  #loop unrolling
+  for (i in 1:num.random.samples) {
+    list.scores <- c()
+    list.scores <- c(list.scores, sample(human.pwm[1:4], 1))
+    list.scores <- c(list.scores, sample(human.pwm[5:8], 1))
+    list.scores <- c(list.scores, sample(human.pwm[9:12], 1))
+    list.scores <- c(list.scores, sample(human.pwm[13:16], 1))
+    list.scores <- c(list.scores, sample(human.pwm[17:20], 1))
+    list.scores <- c(list.scores, sample(human.pwm[21:24], 1))
+    list.scores <- c(list.scores, sample(human.pwm[25:28], 1))
+    list.scores <- c(list.scores, sample(human.pwm[29:32], 1))
+    list.scores <- c(list.scores, sample(human.pwm[33:36], 1))
+    list.scores <- c(list.scores, sample(human.pwm[37:40], 1))
+    list.scores <- c(list.scores, sample(human.pwm[41:44], 1))
+    list.scores <- c(list.scores, sample(human.pwm[45:48], 1))
+    list.scores <- c(list.scores, sample(human.pwm[49:52], 1))
+    
+    raw.data <- c(raw.data, sum(list.scores))
+  }
+  
+  unlisted.data <- unlist(raw.data)
+  
+  hist(
+    unlisted.data,
+    breaks = num.random.samples,
+    xlab = "scores",
+    main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
+  )
+}
