0b5d4ba13ab9db1eb00f2c96ce268795da1750ad
ahtmatrix <alan.han.tu+github@gmail.com>
2016-12-04 05:04:33 +0000

    backup

--- R_scripts/randomPWM.R
+++ R_scripts/randomPWM.R
@@ -16,13 +16,13 @@ human.filename = "rna30upstream_and_CDS.fasta"
 #make a pwm of size 13
 human = readDNAStringSet(human.filename)
 human.kozak = DNAStringSet(subseq(human, start = 22, end = 34))
-
-
+consensusMatrix(human.kozak)
+consensusString(human.kozak)
 
 for (x in 1:2) {
   human.pwm = NULL
   raw.data = NULL
-  unlisted.data = NULL
+  
   if (x == 1) {
     PWM.type = "log2probratio"
   } else if (x == 2) {
@@ -31,7 +31,7 @@ for (x in 1:2) {
   
   human.pwm    = PWM(human.kozak, type = PWM.type)
   
-  num.random.samples = 10000
+  num.random.samples = 100000
   
   raw.data <- numeric(length = num.random.samples)
   
@@ -54,32 +54,33 @@ for (x in 1:2) {
     )
   }))
   
-  unlisted.data <- unlist(raw.data)
-  #par(mfrow = c(1, 2))
   if (x == 1) {
-    hist(
-      unlisted.data,
-      freq = FALSE,
-      breaks = num.random.samples / 15,
-      xlab = "scores",
-      main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
-    )
-    lines(density(unlisted.data), col = "blue", lwd = 2)
-    
+    unlisted.data.1 <- unlist(raw.data)
     
     #lines(density(unlisted.data, adjust = 2), col = "green", lwd = 2, lty = "dotted")
   } else if (x == 2) {
-    hist(
-      unlisted.data,
-      freq = FALSE,
-      breaks = num.random.samples / 15,
-      xlab = "scores",
-      main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
-    )
-    lines(density(unlisted.data), col = "blue", lwd = 2)
-
+    unlisted.data.2 <- unlist(raw.data)
   }
 }
 
 
-lines(density(unlisted.data), col = "blue", lwd = 2)
+hist(
+  unlisted.data.2,
+  freq = FALSE,
+  breaks = num.random.samples / 15,
+  xlab = "scores",
+  main = paste(num.random.samples, "random samples| PWM type = ", PWM.type)
+)
+lines(density(unlisted.data.2), col = "blue", lwd = 2)
+
+# library(plotrix)
+# colors <- c("red", "blue")
+# datafoo <- list(unlisted.data, unlisted.data2)
+# multhist(datafoo, lty = "blank" ,breaks = num.random.samples/100, col = colors, freq = FALSE)
+# lines(density(datafoo[[1]]))
+# legend("topright", c("log2probratio", "prob"), fill = colors)
+#
+#
+#
+#
+#
